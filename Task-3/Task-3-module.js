7900028729b8295fe59ee3c87340e57945a795e5
Hurin Vlad <longsno@gmail.com>
2022-03-15 04:39:44 +0300

    task-3 (changeUser, editTweet, addTweet, validateTweet, getTweet, getTweets) added

---
+++ Task-3/Task-3-module.js
@@ -0,0 +1,103 @@
+import { tweets } from "./Task-3-data.js";
+
+export let module = (function() {
+    let user = 'Petia';
+
+    function getUser() {
+        return user;
+    }
+
+    function getTweet(id) {
+        if(typeof id !== 'string') {
+            console.log('invalid id');
+        }
+        const tweet = tweets.find((e) => e.id === id);
+        return tweet ? tweet : {error : 'id isnt exist'};
+    }
+
+    function validateTweet(tw) {
+        if (!(tw instanceof Object) || tw.constructor !== Object ) {
+            return false;
+        }        
+        const obligatoryKeys = ['id', 'text', 'createdAt', 'author', 'comments'];
+        const twKeys = Object.keys(tw);
+        for (let i = 0; i < obligatoryKeys.length; i++) {
+            if (!twKeys.includes(obligatoryKeys[i])) {
+                return false
+            }
+        }
+        return true;
+    }
+
+    function addTweet(text) {
+        if (typeof text !== 'string' || text.length >= 280) {
+            return false;
+        } 
+        const newTweet = {
+            id: Number(new Date()),
+            text: text,
+            createdAt: new Date().toISOString(),
+            author: user,
+            comments: []
+        }
+        tweets.push(newTweet);
+        return true;
+    }
+
+    function editTweet(id, text) {
+        const tweet = tweets.find((e) => e.id === id);
+        if (typeof text !== 'string' || text.length >= 280 || !tweet) {
+            return false;
+        } 
+        tweet.text = text;
+        return true;
+    }
+
+    function changeUser(newUser) {
+        if (typeof newUser !== 'string' || newUser.length < 4) {
+            return false;
+        }
+        user = newUser;
+        return true;
+    }
+
+    function getTweets(skip = 0, top = 10, filterConfig = {}) {
+        let tweetsCopy = [...tweets];
+        if (filterConfig.author) {
+            tweetsCopy = tweetsCopy.filter((element) => {
+                if (filterConfig.author && filterConfig.author === element.author && filterConfig.author.length !== 0) {
+                    return element;
+                }
+            });
+        }
+        if (filterConfig.text) {
+            tweetsCopy = tweetsCopy.filter((element) => {
+                if (element.text.indexOf(filterConfig.text) !== -1) {
+                    return element;
+                }
+            });
+        }
+        if (filterConfig.hashtags && filterConfig.hashtags.length !== 0) {
+            for(let i = 0; i < filterConfig.hashtags.length; i++) {
+                tweetsCopy = tweetsCopy.filter((tweet) => {
+                    if (tweet.text.indexOf(`${filterConfig.hashtags[i]}`) > 0) {
+                        return tweet;
+                    }
+                });
+            }
+        }
+        if (filterConfig.dateFrom) {
+          console.log(typeof filterConfig.dateFrom)
+        }
+
+        tweetsCopy = [...tweetsCopy];
+        return tweetsCopy.slice(skip, top + skip);
+    }
+
+    console.log(changeUser('maik'));
+    console.log(editTweet('2', 'Вафли бывают красные'));
+    console.log(addTweet('ssssssss'));
+    console.log(validateTweet(tweets[0]));
+    console.log(getTweet('5'));
+    console.log(getTweets(0, 10, {hashtags: ['datamola'], dateFrom: new Date()}));
+}());
